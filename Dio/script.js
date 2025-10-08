dcfcf2bbb650238371a4c8f18cee4a78470b67d6
diogenes-css <diogenesluiz2008@gmail.com>
2025-10-08 09:24:13 -0300

    Add files via upload

---
+++ Dio/script.js
@@ -0,0 +1,43 @@
+// 1)
+// let a = 10
+// let b = 20 
+// alert(a)
+// alert(b)
+// console.log(a,b)
+// document.write(a,b)
+// 2)
+// let a = prompt("digite seu salario")
+// let b =0.5
+// let c= 0.7
+
+// let d= resultado (a,b,c)
+// function resultado (a,b,c){
+//  return  (a + (a*b))- (a*c)
+// }
+// alert ("o valor e " + d)
+
+// 3)
+// let nome = prompt("digite seu NOME")
+// let idade = prompt("digite sua idade ")
+// alert(nome)
+//  alert(idade)
+//  console.log(nome , idade)
+// document.write(nome,idade)
+
+// 4)
+// let a = parseInt(prompt("digite um numero"))
+// let b = parseInt(prompt("digite outro "))
+
+// alert (a+b)
+// alert (a-b)
+// alert (a*b)
+// alert (a/b)
+
+let a = parseInt(prompt("digite um numero"))
+let b = parseInt(prompt("digite outro "))
+let c = resultado (a,b)
+
+function resultado (a,b){
+  return  a+b  a-b a*b a/b
+ }
+ alert ("o valor e " + c)
